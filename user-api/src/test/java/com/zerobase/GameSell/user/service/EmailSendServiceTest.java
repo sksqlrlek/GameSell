85414e7511d9f545bbf2fe736281a98f5951b667
Tamoon <sksqlrlek@gmail.com>
2025-06-05 11:49:07 +0900

    wip: 병합 전 임시 커밋

--- user-api/src/test/java/com/zerobase/GameSell/user/service/EmailSendServiceTest.java
+++ user-api/src/test/java/com/zerobase/GameSell/user/service/EmailSendServiceTest.java
@@ -1,21 +1,19 @@
-package com.zerobase.GameSell.user.service;
-
-import com.zerobase.GameSell.user.service.test.EmailSendService;
-import feign.Response;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest()
-class EmailSendServiceTest {
-
-    @Autowired
-    private EmailSendService emailSendService;
-
-    @Test
-    public void EmailTest() {
-        String response = emailSendService.sendEmail();
-        System.out.println(response);
-    }
-  
-}
+//package com.zerobase.GameSell.user.service;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest()
+//class EmailSendServiceTest {
+//
+//    @Autowired
+//    private EmailSendService emailSendService;
+//
+//    @Test
+//    public void EmailTest() {
+//        String response = emailSendService.sendEmail();
+//        System.out.println(response);
+//    }
+//
+//}
